b4f9e0f9ac11d8e39f920ef8679e99cbed0b6ef4
Tyler Flemming <tflem18@gmail.com>
2018-01-24 00:03:28 -0500

    votes -> hype and a default port other than 3000

--- src/App.js
+++ src/App.js
@@ -4,24 +4,32 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const PHOTOS_QUERY = gql`
-query GetPhotos {
-  photos {
-    id
-    postName
-    meta {
-        votes
+    query GetPhotos {
+        photos {
+            id
+            postName
+            whatToDo
+            meta {
+                hype
+                category
+            }
+        }
     }
-  }
-}
 `;
 
 function App({ data: { loading, photos } }) {
     return (
         <div>
-            {loading ? <p>Loading...</p> :
-            <div>{photos[0].postName} -- {photos[0].meta.votes}</div>}
+            {
+                loading ? <p>Loading...</p> :
+                <div>
+                    <h1>{photos[0].meta.category}</h1>
+                    <div>{photos[0].postName} -- {photos[0].meta.hype}</div>
+                    <span>{photos[0].whatToDo}</span>
+                </div>
+            }
         </div>
     );
-}
+};
 
 export default graphql(PHOTOS_QUERY)(App);
